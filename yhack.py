63c7e2e3a7661d777dd0d69af77c3ab5865219ed
Jake Walsh <jake_walsh@yahoo.com>
2015-11-07 20:56:16 -0500

    payment processing code

--- yhack.py
+++ yhack.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import braintree, random
+import yhackconfig
 
 app = Flask(__name__)
 
@@ -7,16 +9,48 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('trucks/<truck_id>')
-def get_truck_menu(truck_id):
-    return "truck info"
+@app.route('/order/')
+def place_order():
+    clienttoken = braintree.ClientToken.generate()
+    amount = request.args.get('amt')
+    item = request.args.get('item')
 
-@app.route('order/<item_id>')
-def place_order(item_id):
-    return "ordering"
+    return render_template("payment.html", amount=amount, item=item, clienttoken=clienttoken)
+
+
+@app.route('/trucks/3/')
+def get_truck_menu3():
+    return render_template("3.html")
+
+@app.route('/trucks/5/')
+def get_truck_menu3():
+    return render_template("5.html")
+
+@app.route('/trucks/6/')
+def get_truck_menu3():
+    return render_template("6.html")
+
+@app.route('/checkout/', methods=['POST'])
+def checkout():
+
+    result = braintree.Transaction.sale({
+        "amount": request.args.get('amount'),
+        "payment_method_nonce": request.args.get("payment_method_nonce"),
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success:
+        number = random.randrange(1, 999)
+        return render_template("checkout.html", number=number)
+
+    else:
+        return "error"
+
 
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0')
